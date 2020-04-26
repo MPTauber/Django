0d69c66878e39cdd2a6167c7cc722267f33aef48
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-26 10:54:07 -0500

    added comment

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -11,13 +11,12 @@ def index(request):
     return render(request, "learning_logs/index.html")
 
 def topics(request):
-    """The topics page for Learning Log"""
     topics = Topic.objects.order_by("date_added")
     # A context is a dictionary in which the key are names we'll use
     # in the template to access the data, and the values are the data
     # we need to send to the template. In this case, there's only one key-value pair,
     # which contains the set of topics we'll display on the page.
-    context = {"topics":topics}
+    context = {"topics":topics} # value topics (in white) is pointing to "topics=" above, which is holding all information.
     #When building a page that uses data, we pass the context variable to render()
     # as well as the request object and the path to the template
     return render(request, "learning_logs/topics.html", context)
