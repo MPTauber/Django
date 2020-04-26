30a17d44edd6aa39e958dd9c36433c74c4876f12
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-26 10:46:39 -0500

    Add TOpics to views, change Topics in url

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -17,6 +17,6 @@ urlpatterns = {
     # The second argument specifies the function name to call in views.py
     # The third argument provides the name " index" for this URL pattern to refer to it later
     path("", views.index, name="index"),
+    path("topics", views.topics, name="topics"),
     path("", views.base, name="base"),
-    path("", views.topics, name="topics"),
 }
--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
@@ -9,10 +10,19 @@ def index(request):
     """The home page for Learning Log."""
     return render(request, "learning_logs/index.html")
 
+def topics(request):
+    """The topics page for Learning Log"""
+    topics = Topic.objects.order_by("date_added")
+    # A context is a dictionary in which the key are names we'll use
+    # in the template to access the data, and the values are the data
+    # we need to send to the template. In this case, there's only one key-value pair,
+    # which contains the set of topics we'll display on the page.
+    context = {"topics":topics}
+    #When building a page that uses data, we pass the context variable to render()
+    # as well as the request object and the path to the template
+    return render(request, "learning_logs/topics.html", context)
+
 def base(request):
     """The base page for Learning Log."""
     return render(request, "learning_logs/base.html")
 
-def topics(request):
-    """The topics page for Learning Log"""
-    return render(request, "learning_logs/topics.html")
