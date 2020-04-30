46224b9d67af977c9cd2ab3fe35d7eae90f1fa99
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-30 12:39:21 -0500

    Make Registration form

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path, include
+from . import views
 
 app_name = 'users'
 
 urlpatterns = [
-    path('', include('django.contrib.auth.urls'))
+    # INclude default auth urls.
+    path('', include('django.contrib.auth.urls')),
+    # Registration page.
+    path('register/', views.register, name='register'),
 ]
