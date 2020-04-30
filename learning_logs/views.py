d68d82d119a10220f2009454d94a330dd3f33420
Madeleine Tauber <Madeleine_Tauber1@baylor.edu>
2020-04-30 12:07:52 -0500

    create edit_entry

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from .models import Topic
+from .models import Topic, Entry
 from .forms import TopicForm, EntryForm
 
 # Create your views here.
@@ -54,7 +54,6 @@ def new_topic(request):
     context = {'form':form}
     return render(request, 'learning_logs/new_topic.html', context)
 
-## TEST
 def new_entry(request, topic_id):
     topic = Topic.objects.get(id=topic_id)
     if request.method != 'POST':
@@ -74,3 +73,22 @@ def new_entry(request, topic_id):
   
     context = {'form':form, 'topic': topic}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    """Edit an existing entr<"""
+    entry = Entry.objects.get(id= entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        #This argument tells Django to cretae the form prefilled
+        #with information from the existing entry object
+        form = EntryForm(instance=entry)
+    else:
+        #POST data submitted, process data.
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topic', topic_id=topic.id)
+
+    context = {'entry': entry, 'topic': topic, 'form': form}
+    return render(request, 'learning_logs/edit_entry.html', context)
